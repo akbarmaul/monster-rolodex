31225b265f222922d4c73867f346e3d4a082c0cd
akbarmaul <lanang0995@gmail.com>
2019-12-20 13:57:03 +0700

    Updates

---
+++ precache-manifest.070475d84d749d8fb3756bd6e2826c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3589a4ad7728d892da2943b7ac46aad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80baad9b690407feef64",
+    "url": "/monsters-rolodex/static/css/main.296ae87c.chunk.css"
+  },
+  {
+    "revision": "a8b76958ccd5d6d253a3",
+    "url": "/monsters-rolodex/static/js/2.7029a57e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7029a57e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80baad9b690407feef64",
+    "url": "/monsters-rolodex/static/js/main.e4131624.chunk.js"
+  },
+  {
+    "revision": "a565b40e5380e6418ba7",
+    "url": "/monsters-rolodex/static/js/runtime-main.bf85e43f.js"
+  }
+]);
